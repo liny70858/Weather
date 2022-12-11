6eb4d39a15cabfcbde489cab6e08717d1a40f162
Liam Yuan <96031565+liny70858@users.noreply.github.com>
2022-12-11 23:31:16 +0800

    使程序能够正常运行

--- app/src/main/java/com/weather/android/Base/BaseActivity.kt
+++ app/src/main/java/com/weather/android/Base/BaseActivity.kt
@@ -2,10 +2,12 @@ package com.weather.android.Base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.blankj.utilcode.util.FragmentUtils.getSimpleName
+import com.blankj.utilcode.util.LogUtils
 
 open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LogUtils.d("${packageName} is onCreate ")
     }
-
 }
